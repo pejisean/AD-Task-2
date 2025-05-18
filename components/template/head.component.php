a77911d06ce54e9fca77a2eaa0416f6259d362eb
pejisean <202311735@fit.edu.ph>
2025-05-18 17:43:19 +0800

    style - changed echo of page title

--- components/template/head.component.php
+++ components/template/head.component.php
@@ -6,7 +6,7 @@ function head($pageTitle){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?= htmlEscape($pageTitle)?></title>
+        <title><?=$pageTitle?></title>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <link rel="stylesheet" href="/assets/css/example.css">
