9a38e8681798cfd12a1fd25e9835fe6d56f9152d
pejisean <202311735@fit.edu.ph>
2025-05-18 18:34:30 +0800

    feat - added templates ontop

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    require BASE_PATH . '/vendor/autoload.php';
+
+    require_once BASE_PATH . '/components/templates/foot.component.php';
+    require_once BASE_PATH . '/components/templates/head.component.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
